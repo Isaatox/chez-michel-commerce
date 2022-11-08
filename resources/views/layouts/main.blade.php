3e0ac10bb1a5b4e9c7a8d17bd7addb559eba07dd
Isaatox <hugo35340@gmail.com>
2022-11-08 15:01:57 +0100

    Header Responsive

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ url('css/bootstrap.css') }}">
+    <script src="{{ url('js/bootstrap.js') }}"></script>
+    <script src="https://kit.fontawesome.com/b78998cfc3.js" crossorigin="anonymous"></script>
     <title>Chez Michel</title>
 </head>
 
