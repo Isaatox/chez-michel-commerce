0b2a6d25e4f302276ee5a65f95da4b8f92415779
Chantrel.D <dorian.chantrel@hotmail.fr>
2023-02-03 16:35:44 +0100

    Controller meuble par catégorie et view créer

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MeublesParCategorieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,11 @@ Route::get('/Compte', function () {
     return view('Compte');
 });
 
+Route::get('/meubles/{categorie}', [MeublesParCategorieController::class, 'index'])->name('MeublesParCategorie');
+
+
+
+/*! Admin */
 Route::get('/admin/ajouter_meubles',[MeubleController::class,'index'])->name('ajouter_meubles');
 Route::post('/admin/enregistrer_meuble',[MeubleController::class,'enregistrer_meuble'])->name('enregistrer_meuble');
 
